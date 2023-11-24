16a77c34333ac7ae60cd8a3f4cfa2d47edabdc94
Akash09082001 <aakashl@letsupgrade.in>
2023-11-24 17:52:42 +0530

    added new changes

--- public/main.js
+++ public/main.js
@@ -1,4 +1,24 @@
 
+// menu open and close
+
+const mobileMenu = document.getElementById("mobile-nav");
+const openButton = document.getElementById("open");
+const closeButton = document.getElementById("close");
+
+// open menu
+function openMenu() {
+  mobileMenu.style.display = "flex";
+  openButton.style.display = "none";
+  closeButton.style.display = "block";
+}
+function closeMenu() {
+  mobileMenu.style.display = "none";
+  openButton.style.display = "block";
+  closeButton.style.display = "none";
+}
+
+
+
 // image view modal
 const imgModal = document.getElementById("imgModal");
 const showImg = document.getElementById("showImg");
